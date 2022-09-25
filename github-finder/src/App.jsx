8d00eb53608cc179c707bd2e7268ea036d67acb1
KonstantinaStrantzali <konstrantz@gmail.com>
2022-09-25 20:18:22 +0000

    project's core pages created

--- github-finder/src/App.jsx
+++ github-finder/src/App.jsx
@@ -1,17 +1,24 @@
-import React from "react";
- 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from "./components/layout/Navbar"
 import Footer from './components/layout/Footer'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import NotFound from './components//pages/NotFound'
  
 function App () {
   return (
     <>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" component={Navbar} />
-        </Routes>
+        <main className="container mx-auto px-3 pb-12">
+             
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
         <Footer />
       </Router>
     </>
