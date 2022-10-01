7c74bee61ee7e6ec2dc934e26a0645a30d0494f0
KonstantinaStrantzali <konstrantz@gmail.com>
2022-10-01 08:31:10 +0000

    cleanup fetch users

--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { useEffect, useContext } from 'react'
+import { useContext } from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
 import GithubContext from '../context/github/GithubContext'
@@ -8,12 +8,9 @@ import GithubContext from '../context/github/GithubContext'
 
 function UserResults() {
 
-  const {users,loading, fetchUsers} = useContext(GithubContext)
+  const {users,loading} = useContext(GithubContext)
 
-    useEffect(() => {
-        fetchUsers()
-    },[])
-
+    
    
   if(!loading) {
   return (
